14ac43be1e4fc6b96819be5e15cab07e10634945
Steven Gerardo Alvarado Aguiar <alvaradosteven6@gmail.com>
2023-11-25 15:48:53 -0600

    New changes

---
+++ JavaScript/Leccion01/01-01-HolaMundo.js
@@ -0,0 +1,5 @@
+var nombre = "Ubaldo";
+
+console.log(nombre);
+
+
---
+++ JavaScript/Leccion02/02-01-TiposDatosJavaScript.js
@@ -0,0 +1,17 @@
+/*Ejemplo de tipos de datos
+ */
+//Tipo de dato String
+var nombre = "Carlos";
+console.log(nombre);
+
+//Tipo de dato numerico
+var numero = 100;
+console.log(numero);
+
+//Tipo de dato objet
+var objeto = {
+    nombre: "Juan",
+    apellido: "Perez",
+    telefono: "55443322"
+}
+console.log(objeto);
